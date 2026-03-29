5ff848e38d716a32b6d551d05a35baa72c18b5b7
Arya <arya14.doshi@gmail.com>
2026-03-29 20:06:51 +0530

    deleted some files

--- diagnose.py
+++
@@ -1,6 +0,0 @@
-import mediapipe as mp
-print(f"MediaPipe attributes: {dir(mp)}")
-try:
-    print(f"MediaPipe solutions: {mp.solutions}")
-except AttributeError:
-    print("MediaPipe has no attribute 'solutions'")
